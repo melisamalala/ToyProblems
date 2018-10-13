383214549befe2dbc9fd19df7baa9b749b2da7e5
melisamalala <melissamalala@gmail.com>
2018-10-13 15:44:20 +0300

    Added youtube links to my solution

---
+++ codewars/8kyu/removestringspaces.py
@@ -0,0 +1,11 @@
+# Simple, remove the spaces from the string, then return the resultant string.
+
+Video:https://www.youtube.com/watch?v=_STzKe1pTTA&feature=youtu.be
+
+# Codewars Link: https://www.codewars.com/kata/remove-string-spaces/train/python
+
+def no_space(x):
+#   BDD : remove spaces from a string, return the resultant string
+
+    x = x.replace(" ", "")
+    return x
