379177da539fd3f8bd6683adee17774df6802124
melisamalala <melissamalala@gmail.com>
2018-10-13 14:12:05 +0300

    Added url

--- codewars/8kyu/exclamationfour.py
+++ codewars/8kyu/exclamationfour.py
@@ -1,5 +1,4 @@
-#  Exclamation marks series 4: Remove all exclamation marks from sentence
-# #but ensure a exclamation mark at the end of string
+#  Exclamation marks series 4: Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string
 
 # Description:
 # Remove all exclamation marks
@@ -7,6 +6,8 @@
 # For a beginner kata, you can assume that the input
 # data is always a non empty string, no need to verify it.
 
+
+# url: https://www.codewars.com/kata/exclamation-marks-series-number-4-remove-all-exclamation-marks-from-sentence-but-ensure-a-exclamation-mark-at-the-end-of-string/train/python
 
 # Solution:
 
