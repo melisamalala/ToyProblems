be0dad4dad5f1ba4723cb1c738357b43e819a673
melisamalala <melissamalala@gmail.com>
2018-10-13 15:25:05 +0300

    Invert Values

---
+++ codewars/8kyu/invertvalues.py
@@ -0,0 +1,21 @@
+# Invert values
+
+# Description:
+#Given a set of numbers,
+#  return the additive inverse of each.
+# Each positive becomes negatives,
+# and the negatives become positives.
+
+# BDD:
+
+
+# SOLUTION:
+
+def invert(lst):
+    list = []
+    for item in lst:
+        item = item * -1
+        list.append(item)
+    return list
+
+print(invert([1, 2, 3, -4, -5]))
