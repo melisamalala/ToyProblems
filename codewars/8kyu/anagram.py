b7825e67bf28f394d69eb24d90e6427bc68053a0
melisamalala <melissamalala@gmail.com>
2018-10-15 00:16:28 +0300

    anagram

--- codewars/8kyu/anagram.py
+++ codewars/8kyu/anagram.py
@@ -5,4 +5,43 @@ def anagram (s1, s2):
     s1 = s1.replace(" ", "").lower()
     s2 = s1.replace(" ", "").lower()
 
+    # Check if the sorted s1 and s2 are similar
     return sorted (s1) == sorted(s2)
+
+
+def anagram (s1, s2):
+
+
+    # Remove spaces and lowercase
+    s1 = s1.replace(" ", "").lower()
+    s2 = s1.replace(" ", "").lower()
+
+    # Check if same number of letters
+    if len(s1) != len(s2):
+        return False
+
+    # count frequency of each letter
+    count = {}
+
+    for letter in s1:
+        # for every letter in first string
+        if letter in count:
+            count [letter] +=1
+
+        else:
+            count [letter] = 1
+
+    for letter in s2:
+        if letter in count:
+            count[letter] -= 1
+
+        else:
+            count[letter] = 1
+
+    for k in count:
+        if count[k] != 0
+            return False
+
+    return True
+
+
