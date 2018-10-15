e59311534d4be9a3a4b1dd9d99df521574ad81cf
melisamalala <melissamalala@gmail.com>
2018-10-15 17:52:27 +0300

    are they the same

--- codewars/6kyu/are-they-the-same.py
+++ codewars/6kyu/are-they-the-same.py
@@ -7,3 +7,12 @@ def comp(array1, array2):
         x = [i ** 2 for i in array1]
         return sorted(x) == sorted(array2)
 
+
+    '''       
+    SOLUTION 2:
+    '''
+
+
+def comp(array1, array2):
+    # your code
+	return False if array1 == None or array2 == None else sorted([i**2 for i in array1]) == sorted(array2)
