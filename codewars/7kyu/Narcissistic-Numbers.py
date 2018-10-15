b42a00763396cc0cc3231a1bb12c8fa0e053d1f5
melisamalala <melissamalala@gmail.com>
2018-10-15 17:38:08 +0300

    narcissistic

--- codewars/7kyu/Narcissistic-Numbers.py
+++ codewars/7kyu/Narcissistic-Numbers.py
@@ -6,5 +6,12 @@ Ex: 153, where n = 3 (number of digits in 153)
 
 Write a method is_narcissistic(i) (in Haskell: isNarcissistic :: Integer -> Bool) which returns whether or not i is a Narcissistic Number.
 '''
-
 def is_narcissistic(i):
+   n = len(str(i))
+   x= map(int,str(i))
+   narcist = 0
+   for b in x:
+       narcist+=(b**n)
+       if narcist == i:
+           return True
+   return False
