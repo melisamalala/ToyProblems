08f529f3ccde7c35880c507c810e932fe0efc648
melisamalala <melissamalala@gmail.com>
2018-10-15 16:03:08 +0300

    Fibonacci

--- codewars/8kyu/fibonacci.py
+++ codewars/8kyu/fibonacci.py
@@ -1,20 +1,28 @@
-from functools import lru_cache
-# least recently used cache
+# from functools import lru_cache
+# # least recently used cache
+#
+#
+# @lru_cache(maxsize=1000)
+# def fibonacci(n):
+#
+#     # IF WE HAVE CACHED THE VALUE, THEN RETURN IT
+#
+#     # Compute the nth term
+#     if n==1:
+#         return 1
+#     elif n == 2:
+#         return  1
+#     elif n > 2:
+#         return fibonacci(n-1) + fibonacci(n-2)
+#
+#
+# for n in range(1, 5000):
+#         print (n, ":", fibonacci(n))
 
 
-@lru_cache(maxsize=1000)
-def fibonacci(n):
-
-    # IF WE HAVE CACHED THE VALUE, THEN RETURN IT
-
-    # Compute the nth term
-    if n==1:
+def fib  (n):
+    if n ==1 or n ==2:
         return 1
-    elif n == 2:
-        return  1
-    elif n > 2:
-        return fibonacci(n-1) + fibonacci(n-2)
+    return fib (n-1) + fib (n-2)
 
-
-for n in range(1, 501):
-        print (n, ":", fibonacci(n))
+print (fib (20))
