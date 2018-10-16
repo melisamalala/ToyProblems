d5d854cd7f3831eb06fbfe140a944c17ad22867d
melisamalala <melissamalala@gmail.com>
2018-10-16 12:36:24 +0300

    How much water do i need

---
+++ codewars/8kyu/how-much-water-do-i-need.py
@@ -0,0 +1,19 @@
+# VICTORS SOLUTION:
+
+import math
+
+# VICTORS COMPREHENDED SOLUTION:
+def how_much_water(W, L, C):
+    return  ['Too much clothes' if C > 2 * L else 'Not enough clothes' if C < L else round((math.pow(1.1, C - L) * W), 2)][0]
+
+
+   # WELL- EXPLAINED CODE
+
+    if C > 2 * L:
+        return 'Too much clothes'
+    elif C < L:
+        return 'Not enough clothes'
+    else:
+        r = C - L
+        return round((math.pow(1.1, r) * W), 2)
+    # Good luck!
